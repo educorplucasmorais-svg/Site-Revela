9f414ce57de907616eee3d3226cad27fd4189e9e
educorplucasmorais-svg <educorp.lucasmorais@gmail.com>
2025-12-02 03:42:18 -0300

    fix: Use named imports for Kaia components

--- src/pages/Kaia.tsx
+++ src/pages/Kaia.tsx
@@ -1,15 +1,14 @@
-﻿import React from 'react';
-import '../styles/kaia.css';
-import KaiaHero from '../components/KaiaHero';
-import KaiaFeatures from '../components/KaiaFeatures';
-import KaiaPricing from '../components/KaiaPricing';
-import KaiaFAQ from '../components/KaiaFAQ';
-import KaiaCTA from '../components/KaiaCTA';
-import KaiaSecurity from '../components/KaiaSecurity';
-import KaiaTestimonials from '../components/KaiaTestimonials';
+﻿import '../styles/kaia.css';
+import { KaiaHero } from '../components/KaiaHero';
+import { KaiaFeatures } from '../components/KaiaFeatures';
+import { KaiaPricing } from '../components/KaiaPricing';
+import { KaiaFAQ } from '../components/KaiaFAQ';
+import { KaiaCTA } from '../components/KaiaCTA';
+import { KaiaSecurity } from '../components/KaiaSecurity';
+import { KaiaTestimonials } from '../components/KaiaTestimonials';
 import WhatsAppButton from '../components/WhatsAppButton';
 
-const KaiaPage: React.FC = () => {
+const KaiaPage = () => {
   return (
     <div className="kaia-page" style={{ position: 'relative' }}>
       {/* Background */}
