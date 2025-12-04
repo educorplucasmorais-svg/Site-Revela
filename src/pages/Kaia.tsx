f2f3c30efc525951a22d4ea9254c4aadf825ae2e
educorplucasmorais-svg <educorp.lucasmorais@gmail.com>
2025-12-04 06:50:09 -0300

    refactor: simplify Kaia page to match Revela LP architecture

--- src/pages/Kaia.tsx
+++ src/pages/Kaia.tsx
@@ -1,8 +1,34 @@
 ﻿import '../styles/kaia.css';
-import { useEffect, useState, useRef } from 'react';
-import { KaiaWaterLogo } from '../components/KaiaWaterLogo';
+import { KaiaHero } from '../components/KaiaHero';
+import { KaiaBanner } from '../components/KaiaBanner';
+import { KaiaFeatures } from '../components/KaiaFeatures';
 import { KaiaPricing } from '../components/KaiaPricing';
 import { KaiaFAQ } from '../components/KaiaFAQ';
+import { KaiaCTA } from '../components/KaiaCTA';
+
+export default function Kaia() {
+  return (
+    <div className="kaia-page">
+      {/* Hero Section */}
+      <KaiaHero />
+
+      {/* Banner / Intro */}
+      <KaiaBanner />
+
+      {/* Features Section */}
+      <KaiaFeatures />
+
+      {/* Pricing Section */}
+      <KaiaPricing />
+
+      {/* FAQ Section */}
+      <KaiaFAQ />
+
+      {/* CTA Final */}
+      <KaiaCTA />
+    </div>
+  );
+}
 
 // ========================================
 // TECH-NOIR LANDING PAGE - KAIA
